b4cb2ecb10269e62ebbcb367c0d48232fa336f90
Ramez Bariekzahy <ramez@seznam.cz>
2023-12-25 17:06:30 +0100

    spring project initialized

---
+++ aramesh-eshop/src/main/java/cz/aramesh/aramesheshop/ArameshEshopApplication.java
@@ -0,0 +1,13 @@
+package cz.aramesh.aramesheshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArameshEshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArameshEshopApplication.class, args);
+    }
+
+}
---
+++ aramesh-eshop/src/test/java/cz/aramesh/aramesheshop/ArameshEshopApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.aramesh.aramesheshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArameshEshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
